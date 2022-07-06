ab7c104b200534f9857cd9f09612bf47b2f8ac28
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-06 01:27:40 +0100

    Created updateProduct function
    - Function works, products get updated but returns a 400 response and an empty object in postman. WIll need to revisit this once i have finished the tag controller functions as it may be dependant on tag CRUD operations to work as expected

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -53,7 +53,46 @@ const createProduct = async (req, res) => {
 		});
 };
 
-// const updateProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+	// update product data
+	await Product.update(req.body, {
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((product) => {
+			// find all associated tags from ProductTag
+			return ProductTag.findAll({ where: { product_id: req.params.id } });
+		})
+		.then((productTags) => {
+			// get list of current tag_ids
+			const productTagIds = productTags.map(({ tag_id }) => tag_id);
+			// create filtered list of new tag_ids
+			const newProductTags = req.body.tagIds
+				.filter((tag_id) => !productTagIds.includes(tag_id))
+				.map((tag_id) => {
+					return {
+						product_id: req.params.id,
+						tag_id,
+					};
+				});
+			// figure out which ones to remove
+			const productTagsToRemove = productTags
+				.filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+				.map(({ id }) => id);
+
+			// run both actions
+			return Promise.all([
+				ProductTag.destroy({ where: { id: productTagsToRemove } }),
+				ProductTag.bulkCreate(newProductTags),
+			]);
+		})
+		.then((updatedProductTags) => res.json(updatedProductTags))
+		.catch((err) => {
+			// console.log(err);
+			res.status(400).json(err);
+		});
+};
 
 // const deleteProduct = async (req, res) => {};
 
@@ -61,6 +100,6 @@ module.exports = {
 	getAllProducts,
 	getProductById,
 	createProduct,
-	// updateProduct,
+	updateProduct,
 	// deleteProduct,
 };
