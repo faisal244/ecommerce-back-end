e6ff03186eaf690488b453ae89072d982a952f54
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-06 22:29:19 +0100

    Completed getTagById and createTag functions

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -13,10 +13,36 @@ const getAllTags = async (req, res) => {
 	}
 };
 
-// Get tag by ID function
-const getTagById = async (req, res) => {};
+// get tag by ID function
+const getTagById = async (req, res) => {
+	try {
+		const data = await Tag.findByPk(req.params.id);
+		if (data) {
+			return res.json({ success: true, data });
+		}
+		return res
+			.status(404)
+			.json({ success: false, error: "Tag does not exist" });
+	} catch (err) {
+		logError("GET Tags", err.message);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to send response" });
+	}
+};
 
-const createTag = async (req, res) => {};
+// create tag function
+const createTag = async (req, res) => {
+	try {
+		await Tag.create(req.body);
+		return res.json({ success: true, data: "Successfully Created Tag" });
+	} catch (error) {
+		logError("POST Tag", error.message);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to send response" });
+	}
+};
 
 // update tag function
 const updateTag = async (req, res) => {
