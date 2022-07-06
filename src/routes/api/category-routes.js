a0e1ae5b4abebe926061dea74c6dbc8ca82d88f2
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-06 19:18:15 +0100

    Defined all category routes

--- src/routes/api/category-routes.js
+++ src/routes/api/category-routes.js
@@ -15,21 +15,17 @@ router.get("/", getAllCategories);
 // be sure to include its associated Products
 // add try catch statements to each
 
-router.get("/:id", (req, res) => {
-	// find one category by its `id` value
-	// be sure to include its associated Products
-});
+router.get("/:id", getCategoryById);
+// find one category by its `id` value
+// be sure to include its associated Products
 
-router.post("/", (req, res) => {
-	// create a new category
-});
+router.post("/", createNewCategory);
+// create a new category
 
-router.put("/:id", (req, res) => {
-	// update a category by its `id` value
-});
+router.put("/:id", updateCategory);
+// update a category by its `id` value
 
-router.delete("/:id", (req, res) => {
-	// delete a category by its `id` value
-});
+router.delete("/:id", deleteCategory);
+// delete a category by its `id` value
 
 module.exports = router;
